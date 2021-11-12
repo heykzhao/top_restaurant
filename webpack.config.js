4c57003a0115c82ee9224a51218bd23b7946e33b
Kevin Zhao <kvnzhao@outlook.com>
2021-11-12 11:36:16 -0500

    Adjust mobile viewport issue, and header/footer font sizes

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,11 @@ module.exports = {
           title: 'Just Simple Cafe',
           templateContent: `
             <html>
+                <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            </head>
             <body>
                 <div id="content"></div>
             </body>
