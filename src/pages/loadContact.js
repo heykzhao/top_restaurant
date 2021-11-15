7905556bda01a7286470671ca1a9080b6a2bd0d1
Kevin Zhao <kvnzhao@outlook.com>
2021-11-15 12:30:41 -0500

    Start building contact page

--- src/pages/loadContact.js
+++ src/pages/loadContact.js
@@ -6,7 +6,19 @@ function clearBodyContainer() {
 
 function loadContactPage() {
     clearBodyContainer();
-    console.log('Load contact page');
+
+    const bodyContainer = document.querySelector('.body-container');
+    const contactContainer = document.createElement('div');
+    const informationContainer = document.createElement('div');
+    const feedbackContainer = document.createElement('div');
+
+    contactContainer.setAttribute('class', 'contact-container');
+    informationContainer.setAttribute('class', 'information-container');
+    feedbackContainer.setAttribute('class', 'feedback-container');
+
+    bodyContainer.appendChild(contactContainer);
+    contactContainer.appendChild(informationContainer);
+    contactContainer.appendChild(feedbackContainer);
 };
 
 export default loadContactPage;
