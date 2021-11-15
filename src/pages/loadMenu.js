75014c1c16387b0bc10e66bc0a5dacf9a7448068
Kevin Zhao <kvnzhao@outlook.com>
2021-11-15 11:43:10 -0500

    Add function to create drinks and snacks on menu

--- src/pages/loadMenu.js
+++ src/pages/loadMenu.js
@@ -3,9 +3,69 @@ function clearBodyContainer() {
     bodyContainer.innerHTML = '';
 }
 
+function createMenuItemDrink(title, description, price) {
+    const newMenuItem = document.createElement('div');
+    const menuDrinks = document.querySelector('.menu-items-drinks');
+    newMenuItem.setAttribute('class', 'menu-item');
+    newMenuItem.innerHTML = `
+    <div class="menu-title">${title}</div>
+    <div class="menu-description">${description}</div>
+    <div class="menu-price">${price}</div>
+    `;
+    menuDrinks.appendChild(newMenuItem);
+}
+
+function createMenuItemSnack(title, description, price) {
+    const newMenuItem = document.createElement('div');
+    const menuSnacks = document.querySelector('.menu-items-snacks');
+    newMenuItem.setAttribute('class', 'menu-item');
+    newMenuItem.innerHTML = `
+    <div class="menu-title">${title}</div>
+    <div class="menu-description">${description}</div>
+    <div class="menu-price">${price}</div>
+    `
+    menuSnacks.appendChild(newMenuItem);
+}
+
 function loadMenuPage() {
     clearBodyContainer();
-    console.log('Load menu page');
+
+    const bodyContainer = document.querySelector('.body-container');
+    const menu = document.createElement('div');
+    const menuWrapper = document.createElement('div');
+    const menuDrinks = document.createElement('div');
+    const menuSnacks = document.createElement('div');
+
+    menu.setAttribute('class','menu-container');
+    menuWrapper.setAttribute('class','menu-wrapper');
+    menuDrinks.setAttribute('class', 'menu-drinks');
+    menuSnacks.setAttribute('class', 'menu-snacks');
+
+    menuDrinks.innerHTML = `
+    <div class='menu-section-title-drinks'>
+        ━━━━━  DRINKS  ━━━━━
+    </div>
+    <div class='menu-items-drinks'></div>
+    `
+    menuSnacks.innerHTML = `
+    <div class='menu-section-title-snacks'>
+        ━━━━━  SNACKS  ━━━━━
+    </div>
+    <div class='menu-items-snacks'></div>
+    `
+
+    bodyContainer.appendChild(menu);
+    menu.appendChild(menuWrapper);
+    menuWrapper.appendChild(menuDrinks);
+    menuWrapper.appendChild(menuSnacks);
+
+    createMenuItemDrink('test', 'test', 'test');
+    createMenuItemDrink('test', 'test', 'test');
+    createMenuItemDrink('test', 'test', 'test');
+    createMenuItemSnack('test', 'test', 'test');
+    createMenuItemSnack('test', 'test', 'test');
+    createMenuItemSnack('test', 'test', 'test');
+    createMenuItemSnack('test', 'test', 'test');
 };
 
 export default loadMenuPage;
