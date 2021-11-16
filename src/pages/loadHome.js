3ffab2397ddc552aa1cca62c1201736b16973aa5
Kevin Zhao <kvnzhao@outlook.com>
2021-11-16 12:30:47 -0500

    Finish homepage

--- src/pages/loadHome.js
+++ src/pages/loadHome.js
@@ -9,10 +9,23 @@ function clearBodyContainer() {
 function loadHomePage() {
     clearBodyContainer();
     const bodyContainer = document.querySelector('.body-container');
-    const coffeeShopBackgroundImage = document.createElement('div');
-    coffeeShopBackgroundImage.setAttribute('class','home-background-image noselect');
-    coffeeShopBackgroundImage.style.backgroundImage = `url(${Background})`;
-    bodyContainer.appendChild(coffeeShopBackgroundImage);
+    const homeContainer = document.createElement('div');
+    const homeFocus = document.createElement('div');
+
+    homeContainer.setAttribute('class','home-background-image noselect');
+    homeContainer.style.backgroundImage = `url(${Background})`;
+    homeFocus.setAttribute('class', 'home-focus');
+
+    homeFocus.innerHTML = `
+    <div class='home-header'>WELCOME</div>
+    <div class='home-subheader'>Come work, play, relax.</div>
+    <div class='home-subtext'>Enjoy our incredibly delicious coffee, tea, and snacks. Stay however long you want. </div>
+    <button class='menu-CTA'>View Menu ►</button>
+    `
+
+    bodyContainer.appendChild(homeContainer);
+    homeContainer.appendChild(homeFocus);
+
 };
 
 export default loadHomePage;
